06864c78716feb69747c8233e1a2c44249141158
Vblaze <dmin138p@gmail.com>
2025-10-16 16:28:31 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,7 +5,7 @@ const config: CapacitorConfig = {
   appName: 'impostor-insight',
   webDir: 'dist',
   server: {
-    url: 'https://9a2b845c-2dc5-4c1c-8717-959c6e38c1a0.lovableproject.com?forceHideBadge=true',
+    url: 'https://school-who.netlify.app/',
     cleartext: true
   }
 };
